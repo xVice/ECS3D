acffdc15b3f0fc001d3816edf4987ef298a3a7d4
xVice <justus.hanschi@gmail.com>
2023-08-11 21:18:38 +0200

    Removed a small test thingy

--- OpenGLForm.cs
+++ OpenGLForm.cs
@@ -12,8 +12,6 @@ namespace ECS3D
     {
         private Engine ecsEngine;
 
-        private List<CameraComponent> cameras = new List<CameraComponent>();
-
         public OpenGLForm()
         {
             InitializeComponent();
@@ -22,8 +20,7 @@ namespace ECS3D
         {
             ecsEngine = new Engine(glControl1);
             ecsEngine.Create3DObj("./teapot.obj", new Vector3(0, 0f, 0f));
-            cameras.Add(ecsEngine.CreateCamera("Camera1", new Vector3(0, 0, -15)));
-            ecsEngine.SetActiveCamera(cameras[0]);
+            ecsEngine.SetActiveCamera(ecsEngine.CreateCamera("Camera1", new Vector3(0, 0, -15)));
             ecsEngine.Awake();
         }
 
@@ -44,28 +41,6 @@ namespace ECS3D
         private void glControl1_KeyDown(object sender, KeyEventArgs e)
         {
             ecsEngine.MoveCam(e.KeyCode);
-
-            if(e.KeyCode == Keys.C)
-            {
-                var newCam = ecsEngine.CreateCamera(Guid.NewGuid().ToString(), ecsEngine.activeCamera.position);
-                newCam.yaw = ecsEngine.activeCamera.yaw;
-                newCam.pitch = ecsEngine.activeCamera.pitch;
-                newCam.front = ecsEngine.activeCamera.front;
-                newCam.up = ecsEngine.activeCamera.up;
-                cameras.Add(newCam);
-            }
-            else if(e.KeyCode == Keys.N)
-            {
-                var nextCamIndex = cameras.IndexOf(ecsEngine.activeCamera) + 1;
-                if (cameras.Count() - 1 > nextCamIndex)
-                {
-                    ecsEngine.SetActiveCamera(cameras[nextCamIndex]);
-                }
-                else
-                {
-                    ecsEngine.SetActiveCamera(cameras[0]);
-                }
-            }
         }
 
         private void glControl1_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
